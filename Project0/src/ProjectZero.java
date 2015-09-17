adcefc647e39773614e746f264d575f86dac6ead
ausby93 <ausby93@gmail.com>
2015-09-17 18:39:21 -0400

    Update ProjectZero.java

--- Project0/src/ProjectZero.java
+++ Project0/src/ProjectZero.java
@@ -1,21 +1,41 @@
 import java.util.Scanner;
 
+/**
+ * 
+ * @author Corey Ausby
+ *
+ */
+
 public class ProjectZero {
 
 	public static void main(String[] args) {
 		
-			Scanner inputSource = new Scanner(System.in);
-			int input1, input2, result;
+		public static void main(String[] args) {
+		Scanner inputSource = new Scanner(System.in);
+		Scanner runAgain = new Scanner(System.in); //reading in another input so the program can run again
+		
+		
+		String answer = null;
+	
+	do { // do-while loop so the program can be run over and over
+		
+			System.out.println("Please enter two numbers: ");
 			
-			System.out.print("Please enter two numbers of your exalted choosing: ");
-			input1 = inputSource.nextInt();
-			input2 = inputSource.nextInt();
+		double input1  = Double.parseDouble(inputSource.next());
+		double input2 = Double.parseDouble(inputSource.next());
+		double result = input1 + input2;
 			
-			result = input1 + input2;
-			System.out.println(result);
+		 System.out.println("The sum of " + input1 + " and " + input2 + " is " + result);
+
+		    
+		    System.out.println("Do you want to run this again? Enter Y for Yes or N for No: ");
+		    answer = runAgain.next();
 			
-			inputSource.close();
-			
+		
+	} while (!answer.equals("N"));
+		
+	    inputSource.close();		
+
 	}
 
 }
